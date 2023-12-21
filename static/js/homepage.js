f13097b7624875697bdb9b31795f90ff7c7f372d
Michael Babiy <mr.babiymichael@gmail.com>
2023-12-21 16:19:21 +0200

    homePage 1.0

---
+++ static/js/homepage.js
@@ -0,0 +1,19 @@
+// Pause and play the video, and change the button text
+function videoPlayPause() {
+    // Get the video
+    var video = document.getElementById("myVideo");
+  
+    // Get the button
+    var btn = document.getElementById("myBtn");
+    if (video.paused)
+    {
+      video.play();
+      btn.innerHTML = "Pause";
+    }
+    else
+    {
+      video.pause();
+      btn.innerHTML = "Play";
+    }
+  }
+  
