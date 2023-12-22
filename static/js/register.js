497fdf1a2640ab9886a103ed6d7931496f7f9f99
Yuval <yuvalcohen1701@gmail.com>
2023-12-22 16:33:00 +0200

    registe.js ver 2

--- static/js/register.js
+++ static/js/register.js
@@ -2,12 +2,30 @@ function checkInput(){
     let user = document.querySelector("#username").value;
     if (user.length < 4 || user.length > 12)
     {
-        alert("Username problem");
+        alert("Username must be between 4 to 12 character");
     }
     let pass = document.querySelector("#password").value;
-    let passagain = document.querySelector("#passwordagain").value;
-    if (pass != passagain || pass.length < 3)
+    if (pass.length < 4 || pass.length > 12)
     {
-        alert("Pass problem");
+        alert("Password must be between 4 to 12 character");
     }
+    if (!containsNumbers(pass)) 
+    {
+        alert("Password must contain at least one number")
+    }
+    if (!hasLetter(pass))
+    {
+        alert("Password must contain at least one letter")
+    }
+    let passagain = document.querySelector("#passwordagain").value;
+    if (pass != passagain)
+    {
+        alert("Password and confirm must match");
+    }
+}
+function containsNumbers(str) {
+    return /\d/.test(str);
+}
+function hasLetter(str) {
+    return str.match(/[a-z]/);
 }
