67f2713dba013ecd6dc5c45d2fd754e4395bb721
Yuval <yuvalcohen1701@gmail.com>
2023-12-27 11:08:18 +0200

    register  email, phone, db courts abd rents

--- static/js/register.js
+++ static/js/register.js
@@ -1,7 +1,8 @@
 function checkInput(){
     let user = document.querySelector("#username").value;
     let pass = document.querySelector("#password").value;
-    let passagain = document.querySelector("#passwordagain").value;
+    let email = document.querySelector("#email").value;
+    let phone = document.querySelector("#phone").value;
     var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
     if (user.length < 4 || user.length > 12)
     {
@@ -9,7 +10,21 @@ function checkInput(){
         document.getElementById("failed").innerHTML="Username must be between 4 and 12 charcters";
         return false;
         
-    }   
+    }
+    else if (phone.length == 0)
+    {
+        myModal.show();
+        document.getElementById("failed").innerHTML="Must write phone";
+        return false;
+        
+    }
+    else if (email.length == 0)
+    {
+        myModal.show();
+        document.getElementById("failed").innerHTML="Must write email";
+        return false;
+        
+    }         
     else if (pass.length < 4 || pass.length > 12)
     {
         myModal.show();
