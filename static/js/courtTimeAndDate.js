cc9c65bd44ce312ff29870631010c7450ac0dd7a
Yuval <yuvalcohen1701@gmail.com>
2023-12-29 22:36:31 +0200

    turn off hours in courts

--- static/js/courtTimeAndDate.js
+++ static/js/courtTimeAndDate.js
@@ -5,9 +5,33 @@ window.onload = function () {
     dateElement.max = new Date(Date.now() + twoWeeks).toISOString().split('T')[0];
 }
 
+
+function checkDate(len, time) {
+    var valueDate = document.getElementById('accident').value;
+    if (!valueDate) {
+        var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
+        myModal.show();
+        document.getElementById("failed").innerHTML="Must select date";
+        return false;
+    }
+    changeListenerDate(len, time)
+}
+
+
 function changeListenerDate(len, time) {
     var date = document.getElementById("accident").value;
     console.log(len);
     console.log(date);
     console.log(time);
+    for (let i = 0; i < time.length; i++ ) {
+        if (date == time[i]["date"]) {
+            if (len == 1) {
+                    document.getElementById(time[i]["start_time"].toString()).disabled = true;
+            }
+            else {
+                document.getElementById(time[i]["start_time"].toString()).disabled = true;
+                document.getElementById((time[i]["start_time"] + 1).toString()).disabled = true;
+            }
+        }
+    }
 }
