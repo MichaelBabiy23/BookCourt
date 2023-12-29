fca9ba5cce7cfaa616757373d163a049eaed6ba8
Michael Babiy <mr.babiymichael@gmail.com>
2023-12-29 18:32:34 +0200

    courts-choose sport before court

--- static/js/courts.js
+++ static/js/courts.js
@@ -4,14 +4,13 @@ window.onload = function () {
 }
 
 function changeListenerCourts() {
-
     var value = document.getElementById("court-selector").value;
     var selectobject = document.getElementById("display-court-name");
+    selectobject.disabled = false;
     for (var i = 0; i < selectobject.length; i++) {
         if (selectobject.options[i].id != value)
             selectobject.options[i].style.display = "none";
         else
             selectobject.options[i].style.display = "block";
     }
-    selectobject.selectionIndex = 0;
 }
